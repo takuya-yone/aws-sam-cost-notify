1259257a6719fd98b6f0cca37cbec46dc7a2e73b
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-06-09 03:18:27 +0900

    up

--- cost_notify/app.py
+++ cost_notify/app.py
@@ -4,6 +4,8 @@ import datetime
 import os
 import pandas as pd
 
+from zoneinfo import ZoneInfo
+from tabulate import tabulate
 from urllib.request import Request, urlopen, URLError, HTTPError
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools import Tracer
@@ -12,6 +14,7 @@ tracer = Tracer()
 logger = Logger()
 
 SLACK_WEBHOOK_URL = os.environ['SLACK_WEBHOOK_URL']
+tokyo = ZoneInfo("Asia/Tokyo") 
 
 
 def get_cost(ce_client, start, end) -> list:
@@ -25,7 +28,7 @@ def get_cost(ce_client, start, end) -> list:
         },
         Granularity='DAILY',
         Metrics=[
-            'NetUnblendedCost'
+            'UnblendedCost'
         ],
         GroupBy=[
             {
@@ -36,10 +39,8 @@ def get_cost(ce_client, start, end) -> list:
     )
     # logger.info(response)
     for item in response['ResultsByTime'][0]['Groups']:
-        billings_raw.append({
-            'service_name': item['Keys'][0],
-            'billing': item['Metrics']['NetUnblendedCost']['Amount']
-        })
+        billings_raw.append({'service_name': item['Keys'][0], 'billing': round(
+            float(item['Metrics']['UnblendedCost']['Amount']), 2)})
     billings_sorted = sorted(
         billings_raw,
         key=lambda x: x['billing'],
@@ -48,28 +49,28 @@ def get_cost(ce_client, start, end) -> list:
     return billings_sorted[:10]
 
 
-def generate_slack_message(billing_sorted: list) -> str:
+def generate_slack_message(billing_sorted: list, start: str, end: str) -> str:
+    service_list = [x['service_name'] for x in billing_sorted]
+    billing_list = [x['billing'] for x in billing_sorted]
     df = pd.DataFrame({
-    '名前' :['田中', '山田', '高橋'],
-    '役割' : ['営業部長', '広報部', '技術責任者'],
-    '身長' : [178, 173, 169]
+        'Service': service_list,
+        'Cost': billing_list
     })
-    print(df)
-    # print(df.dtypes)
 
+    dfstr = df.to_string(index_names=False)
+    print(dfstr)
+    # print(df.dtypes)
 
     message = {
         "blocks": [
             {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": df.to_string()
-                }
-            }
-        ],
-
-    }
+                "type": "header", "text": {
+                    "type": "plain_text", "text": "{}〜{}".format(
+                        start, end)}}, {
+                "type": "section", "text": {
+                    "type": "mrkdwn", "text": "```\n{}\n```".format(
+                        tabulate(
+                            df, headers="keys", tablefmt="psql"))}}], }
     return message
 
 
@@ -91,12 +92,14 @@ def send_slack_message(message: str, webhookurl: str) -> None:
 @logger.inject_lambda_context(log_event=False)
 def lambda_handler(event, context) -> None:
 
-    today = datetime.date.today()
-    start = today.replace(day=1).strftime('%Y-%m-%d')
-    end = today.strftime('%Y-%m-%d')
+    today = datetime.datetime.now(tokyo)
+    today_1d = today - datetime.timedelta(days=1)
+    today_2d = today - datetime.timedelta(days=2)
+    start = today_2d.strftime('%Y-%m-%d')
+    end = today_1d.strftime('%Y-%m-%d')
     ce_client = boto3.client('ce', region_name='us-east-1')
     billings_sorted = get_cost(ce_client, start, end)
-    slack_message = generate_slack_message(billings_sorted)
+    slack_message = generate_slack_message(billings_sorted, start, end)
     send_slack_message(slack_message, SLACK_WEBHOOK_URL)
     return None
     # return billings_sorted
